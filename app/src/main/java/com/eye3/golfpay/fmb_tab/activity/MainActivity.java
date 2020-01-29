62e7f135efd08194dc4364e87cad12f1775d6aa5
tike <tikedev>
2020-01-29 18:30:25 +0900

    TeeUpTime 리스트 디스플레이 작업

--- app/src/main/java/com/eye3/golfpay/fmb_tab/activity/MainActivity.java
+++ app/src/main/java/com/eye3/golfpay/fmb_tab/activity/MainActivity.java
@@ -546,7 +546,6 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
     @Override
     public void onBackPressed() {
-        //여기 수정할것
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START);
         } else {
@@ -559,64 +558,36 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     protected void onResume() {
         super.onResume();
         systemUIHide();
-
-        ScoreBoard board = new ScoreBoard();
-
     }
 
     private class TeeUpAdapter extends RecyclerView.Adapter<TeeUpAdapter.TeeUpTimeItemViewHolder> {
-        ArrayList<TodayReserveList> todayReserveList;
 
-        //        protected LinearLayout ll_item;
-//        FmbCustomDialog  fmbDialog;
-        public TeeUpAdapter(Context context, ArrayList<TodayReserveList> todayReserveList) {
+        ArrayList<TodayReserveList> todayReserveList;
+        TextView teeUpTimeTextView, reservationPersonNameTextView;
+
+        TeeUpAdapter(Context context, ArrayList<TodayReserveList> todayReserveList) {
             this.todayReserveList = todayReserveList;
         }
 
-        public class TeeUpTimeItemViewHolder extends RecyclerView.ViewHolder {
-//            protected TextView[] tvPar = new TextView[9];
-//            protected TextView tvRank, tvName;
+        class TeeUpTimeItemViewHolder extends RecyclerView.ViewHolder {
 
-            public TeeUpTimeItemViewHolder(View view) {
+            TeeUpTimeItemViewHolder(View view) {
                 super(view);
-//                tvRank = view.findViewById(R.id.rank);
-//                tvName = view.findViewById(R.id.name);
-//
-//                tvPar[0] = view.findViewById(R.id.hole1);
-//                tvPar[1] = view.findViewById(R.id.hole2);
-//                tvPar[2] = view.findViewById(R.id.hole3);
-//                tvPar[3] = view.findViewById(R.id.hole4);
-//                tvPar[4] = view.findViewById(R.id.hole5);
-//                tvPar[5] = view.findViewById(R.id.hole6);
-//                tvPar[6] = view.findViewById(R.id.hole7);
-//                tvPar[7] = view.findViewById(R.id.hole8);
-//                tvPar[8] = view.findViewById(R.id.hole9);
-//                for(int i = 0 ;  NUM_OF_HOLES > i ; i++) {
-//                    tvPar[i].setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View v) {
-//                            GoNativeScreen(new ScoreInputFragment(), null);
-//                        }
-//                    });
-//                }
+
+                teeUpTimeTextView = view.findViewById(R.id.teeUpTimeTextView);
+                reservationPersonNameTextView = view.findViewById(R.id.reservationPersonNameTextView);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        int pos = getAdapterPosition() ;
+                    }
+                });
+
             }
 
-//            private View.OnClickListener leftListener = new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    fmbDialog.dismiss();
-//                }
-//            };
-
-//            private View.OnClickListener rightListener = new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    //
-//                }
-//            };
         }
 
-        // RecyclerView에 새로운 데이터를 보여주기 위해 필요한 ViewHolder를 생성해야 할 때 호출됩니다.
         @NonNull
         @Override
         public TeeUpTimeItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
@@ -624,25 +595,22 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             return new TeeUpTimeItemViewHolder(view);
         }
 
+        String timeMapper(String time) {
+            String timeString;
+            String amOrPm;
+            if (Integer.parseInt(time.split(":")[0]) >= 12) {
+                amOrPm = " PM";
+            } else {
+                amOrPm = " AM";
+            }
+            timeString = time.split(":")[0] + ":" + time.split(":")[1] + amOrPm;
+            return timeString;
+        }
 
         @Override
-        public void onBindViewHolder(@NonNull TeeUpTimeItemViewHolder scoreItemViewHolder, int i) {
-//            final int pos = i;
-//            for (int k = 0; scoresList.get(i).length > k; k++) {
-//                String[] list = scoresList.get(i);
-//                scoreItemViewHolder.tvRank.setText(list[0]);
-//                scoreItemViewHolder.tvName.setText(list[1]);
-//
-//                scoreItemViewHolder.tvPar[0].setText(list[2]);
-//                scoreItemViewHolder.tvPar[1].setText(list[3]);
-//                scoreItemViewHolder.tvPar[2].setText(list[4]);
-//                scoreItemViewHolder.tvPar[3].setText(list[5]);
-//                scoreItemViewHolder.tvPar[4].setText(list[6]);
-//                scoreItemViewHolder.tvPar[5].setText(list[7]);
-//                scoreItemViewHolder.tvPar[6].setText(list[8]);
-//                scoreItemViewHolder.tvPar[7].setText(list[9]);
-//                scoreItemViewHolder.tvPar[8].setText(list[10]);
-//            }
+        public void onBindViewHolder(@NonNull TeeUpTimeItemViewHolder scoreItemViewHolder, int position) {
+            teeUpTimeTextView.setText(timeMapper(todayReserveList.get(position).getTeeoff()));
+            reservationPersonNameTextView.setText(todayReserveList.get(position).getGuestName());
         }
 
         @Override
