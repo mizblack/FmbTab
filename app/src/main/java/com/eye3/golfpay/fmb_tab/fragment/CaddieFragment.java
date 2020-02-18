91e4f309357aead1560e09bcb30f3511f0ecc30a
tike <tikedev>
2020-02-18 16:39:02 +0900

    사진 촬영, 사인 기능 진행중

--- app/src/main/java/com/eye3/golfpay/fmb_tab/fragment/CaddieFragment.java
+++ app/src/main/java/com/eye3/golfpay/fmb_tab/fragment/CaddieFragment.java
@@ -1,35 +1,60 @@
 package com.eye3.golfpay.fmb_tab.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.icu.text.SimpleDateFormat;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.exifinterface.media.ExifInterface;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.eye3.golfpay.fmb_tab.R;
+import com.eye3.golfpay.fmb_tab.activity.MainActivity;
 import com.eye3.golfpay.fmb_tab.common.Global;
 import com.eye3.golfpay.fmb_tab.model.guest.Guest;
 import com.eye3.golfpay.fmb_tab.util.EditorDialogFragment;
 import com.eye3.golfpay.fmb_tab.util.SignatureDialogFragment;
 import com.eye3.golfpay.fmb_tab.view.CaddieViewGuestItem;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class CaddieFragment extends BaseFragment {
 
     protected String TAG = getClass().getSimpleName();
-    //    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
-//    private static final int REQUEST_IMAGE_CAPTURE = 672;
-//    private String imageFilePath;
-//    private Uri photoUri;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    private String imageFilePath;
+    private Uri photoUri;
     View v;
     private ArrayList<Guest> guestArrayList = Global.guestArrayList;
     private LinearLayout memberLinearLayout;
@@ -83,17 +108,6 @@ public class CaddieFragment extends BaseFragment {
         return v;
     }
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        if (requestCode == MY_PERMISSIONS_REQUEST_CAMERA) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                sendTakePhotoIntent();
-//            } else {
-//                Toast.makeText(getActivity(), "카메라 사용 권한이 없습니다.", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -133,7 +147,7 @@ public class CaddieFragment extends BaseFragment {
         }
     }
 
-    private void guestListOnClick(final CaddieViewGuestItem caddieViewGuestItem, int position) {
+    private void guestListOnClick(final CaddieViewGuestItem caddieViewGuestItem) {
         caddieViewGuestItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -172,7 +186,7 @@ public class CaddieFragment extends BaseFragment {
                 final CaddieViewGuestItem caddieViewGuestItem = new CaddieViewGuestItem(mContext);
                 memberLinearLayout.addView(caddieViewGuestItem);
                 caddieViewGuestItemArrayList.add(caddieViewGuestItem);
-                guestListOnClick(caddieViewGuestItem, i);
+                guestListOnClick(caddieViewGuestItem);
 
                 TextView memberNameTextView = caddieViewGuestItem.findViewById(R.id.memberNameTextView);
                 memberNameTextView.setText(guestArrayList.get(i).getGuestName());
@@ -215,119 +229,133 @@ public class CaddieFragment extends BaseFragment {
         });
     }
 
-//    private void sendTakePhotoIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(Objects.requireNonNull(getActivity()).getPackageManager()) != null) {
-//            File photoFile = null;
-//            try {
-//                photoFile = createImageFile();
-//            } catch (IOException ex) {
-//                // Error occurred while creating the File
-//            }
-//
-//            if (photoFile != null) {
-//                photoUri = FileProvider.getUriForFile(getActivity(), getActivity().getPackageName(), photoFile);
-//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-//
-//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//            }
-//        }
-//    }
 
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+    private File createImageFile() throws IOException {
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = Objects.requireNonNull(getActivity()).getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,      /* prefix */
+                ".jpg",         /* suffix */
+                storageDir          /* directory */
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void sendTakePhotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(Objects.requireNonNull(getActivity()).getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            if (photoFile != null) {
+                photoUri = FileProvider.getUriForFile(getActivity(), getActivity().getPackageName(), photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendTakePhotoIntent();
+            } else {
+                Toast.makeText(getActivity(), "카메라 사용 권한이 없습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 //            Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
-//            ExifInterface exif = null;
-//
-//            try {
-//                exif = new ExifInterface(imageFilePath);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            int exifOrientation;
-//            int exifDegree;
-//
-//            if (exif != null) {
-//                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-//                exifDegree = exifOrientationToDegrees(exifOrientation);
-//            } else {
-//                exifDegree = 0;
-//            }
-//
-////            ((ImageView) v.findViewById(R.id.caddieImageView)).setImageBitmap(rotate(bitmap, exifDegree));
-//        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_CANCELED) {
-//            ((MainActivity) mParentActivity).changeDrawerViewToMenuView();
-//
-//        }
-//    }
+            ExifInterface exif = null;
 
-//    private int exifOrientationToDegrees(int exifOrientation) {
-//        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
-//            return 90;
-//        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
-//            return 180;
-//        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
-//            return 270;
-//        }
-//        return 0;
-//    }
+            try {
+                exif = new ExifInterface(imageFilePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-//    private Bitmap rotate(Bitmap bitmap, float degree) {
-//        Matrix matrix = new Matrix();
-//        matrix.postRotate(degree);
-//        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-//    }
-//
-//    private File createImageFile() throws IOException {
-//        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String imageFileName = "TEST_" + timeStamp + "_";
-//        File storageDir = Objects.requireNonNull(getActivity()).getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//                imageFileName,      /* prefix */
-//                ".jpg",         /* suffix */
-//                storageDir          /* directory */
-//        );
-//        imageFilePath = image.getAbsolutePath();
-//        return image;
-//    }
+            int exifOrientation;
+            int exifDegree;
 
-//    private static File getResizedFile(Context context, Bitmap bitmap, String filename) {
-//        //create a file to write bitmap data
-//        File f = new File(context.getCacheDir(), filename);
-//        try {
-//            f.createNewFile();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        //Convert bitmap to byte array
-//        //  Bitmap bitmap = bitmap;
-//        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 10 /*ignored for PNG*/, bos);
-//        byte[] bitmapData = bos.toByteArray();
-//
-//        //write the bytes in file
-//        FileOutputStream fos = null;
-//        try {
-//            fos = new FileOutputStream(f);
-//            fos.write(bitmapData);
-//            fos.flush();
-//            fos.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+            if (exif != null) {
+                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                exifDegree = exifOrientationToDegrees(exifOrientation);
+            } else {
+                exifDegree = 0;
+            }
 
-//    public static File getAlbumStorageDir(Context context, String albumName) {
-//        // Get the directory for the app's private pictures directory.
-//        File file = new File(context.getExternalFilesDir(
-//                Environment.DIRECTORY_PICTURES), albumName);
-//        if (!file.mkdirs()) {
-//            Log.e("FileUtil", "Directory not created");
-//        }
-//        return file;
-//    }
+//            ((ImageView) v.findViewById(R.id.caddieImageView)).setImageBitmap(rotate(bitmap, exifDegree));
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_CANCELED) {
+            ((MainActivity) mParentActivity).changeDrawerViewToMenuView();
+
+        }
+    }
+
+    private Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private static File getResizedFile(Context context, Bitmap bitmap, String filename) {
+        //create a file to write bitmap data
+        File f = new File(context.getCacheDir(), filename);
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Convert bitmap to byte array
+        //  Bitmap bitmap = bitmap;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 10 /*ignored for PNG*/, bos);
+        byte[] bitmapData = bos.toByteArray();
+
+        //write the bytes in file
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(f);
+            fos.write(bitmapData);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+
+    public static File getAlbumStorageDir(Context context, String albumName) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("FileUtil", "Directory not created");
+        }
+        return file;
+    }
 
 }
