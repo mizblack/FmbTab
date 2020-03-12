5ac1088f3347c70602ef935fa21e16b10c3e0070
tikedev <tike@tikedev.com>
2020-03-12 19:11:09 +0900

    니어리스트/롱기스트 작업 진행중(버그발생), 버그 픽스중

--- app/src/main/java/com/eye3/golfpay/fmb_tab/fragment/NearestLongestDialogFragment.java
+++ app/src/main/java/com/eye3/golfpay/fmb_tab/fragment/NearestLongestDialogFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.eye3.golfpay.fmb_tab.R;
+import com.eye3.golfpay.fmb_tab.common.Global;
+import com.eye3.golfpay.fmb_tab.model.teeup.GuestDatum;
+import com.eye3.golfpay.fmb_tab.view.NearestLongestInputItem;
+
+import java.util.ArrayList;
 
 public class NearestLongestDialogFragment extends DialogFragment {
 
     protected String TAG = getClass().getSimpleName();
+    private ArrayList<GuestDatum> guestArrayList = Global.teeUpTime.getTodayReserveList().get(Global.selectedTeeUpIndex).getGuestData();
+    private LinearLayout guestItemLinearLayout;
+    private ArrayList<NearestLongestInputItem> nearestLongestInputItemArrayList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,9 @@ public class NearestLongestDialogFragment extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fr_nearest_longest, container, false);
+        View view = inflater.inflate(R.layout.fr_nearest_longest, container, false);
+        guestItemLinearLayout = view.findViewById(R.id.guestItemLinearLayout);
+        return view;
     }
 
     @Override
@@ -34,6 +45,31 @@ public class NearestLongestDialogFragment extends DialogFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        if (guestArrayList.size() != 0) {
+            for (int i = 0; i < guestArrayList.size(); i++) {
+                final NearestLongestInputItem nearestLongestInputItem = new NearestLongestInputItem(getContext());
+                nearestLongestInputItemArrayList = new ArrayList<>();
+                nearestLongestInputItemArrayList.add(nearestLongestInputItem);
+                guestItemLinearLayout.addView(nearestLongestInputItem);
+                final View selectorLinearLayout = nearestLongestInputItem.findViewById(R.id.selectorLinearLayout);
+                selectorLinearLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        for (int i = 0; i < nearestLongestInputItemArrayList.size(); i++) {
+                            View selectorLinearLayoutTemp = nearestLongestInputItemArrayList.get(i).findViewById(R.id.selectorLinearLayout);
+                            selectorLinearLayoutTemp.setBackgroundColor(0xffffffff);
+                            View modifyDividerTemp = nearestLongestInputItemArrayList.get(i).findViewById(R.id.modifyDivider);
+                            modifyDividerTemp.setVisibility(View.GONE);
+                        }
+                        selectorLinearLayout.setBackgroundResource(R.drawable.shape_black_edge);
+                        View modifyDivider = nearestLongestInputItem.findViewById(R.id.modifyDivider);
+                        modifyDivider.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }
+
     }
 
 }
