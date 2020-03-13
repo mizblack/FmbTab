6a69df7ef5b0d6ddf923fb4f45f93b49ccb73061
tike <tikedev>
2020-03-13 11:16:11 +0900

    니어리스트/롱기스트 작업 진행중. 버그픽스

--- app/src/main/java/com/eye3/golfpay/fmb_tab/fragment/NearestLongestDialogFragment.java
+++ app/src/main/java/com/eye3/golfpay/fmb_tab/fragment/NearestLongestDialogFragment.java
@@ -1,10 +1,12 @@
 package com.eye3.golfpay.fmb_tab.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,7 +24,8 @@ public class NearestLongestDialogFragment extends DialogFragment {
     protected String TAG = getClass().getSimpleName();
     private ArrayList<GuestDatum> guestArrayList = Global.teeUpTime.getTodayReserveList().get(Global.selectedTeeUpIndex).getGuestData();
     private LinearLayout guestItemLinearLayout;
-    private ArrayList<NearestLongestInputItem> nearestLongestInputItemArrayList;
+    private View cancelLinearLayout;
+    private ArrayList<NearestLongestInputItem> nearestLongestInputItemArrayList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class NearestLongestDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fr_nearest_longest, container, false);
         guestItemLinearLayout = view.findViewById(R.id.guestItemLinearLayout);
+        cancelLinearLayout = view.findViewById(R.id.cancelLinearLayout);
         return view;
     }
 
@@ -49,26 +53,45 @@ public class NearestLongestDialogFragment extends DialogFragment {
         if (guestArrayList.size() != 0) {
             for (int i = 0; i < guestArrayList.size(); i++) {
                 final NearestLongestInputItem nearestLongestInputItem = new NearestLongestInputItem(getContext());
-                nearestLongestInputItemArrayList = new ArrayList<>();
                 nearestLongestInputItemArrayList.add(nearestLongestInputItem);
                 guestItemLinearLayout.addView(nearestLongestInputItem);
                 final View selectorLinearLayout = nearestLongestInputItem.findViewById(R.id.selectorLinearLayout);
+                TextView nameTextView = nearestLongestInputItem.findViewById(R.id.nameTextView);
+                nameTextView.setText(guestArrayList.get(i).getGuestName());
+
                 selectorLinearLayout.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
                         for (int i = 0; i < nearestLongestInputItemArrayList.size(); i++) {
                             View selectorLinearLayoutTemp = nearestLongestInputItemArrayList.get(i).findViewById(R.id.selectorLinearLayout);
                             selectorLinearLayoutTemp.setBackgroundColor(0xffffffff);
                             View modifyDividerTemp = nearestLongestInputItemArrayList.get(i).findViewById(R.id.modifyDivider);
                             modifyDividerTemp.setVisibility(View.GONE);
+                            TextView modifyTextViewTemp = nearestLongestInputItemArrayList.get(i).findViewById(R.id.modifyTextView);
+                            modifyTextViewTemp.setText("수정");
+                            modifyTextViewTemp.setTextColor(0xffcccccc);
                         }
+
                         selectorLinearLayout.setBackgroundResource(R.drawable.shape_black_edge);
                         View modifyDivider = nearestLongestInputItem.findViewById(R.id.modifyDivider);
                         modifyDivider.setVisibility(View.VISIBLE);
+                        TextView modifyTextView = nearestLongestInputItem.findViewById(R.id.modifyTextView);
+                        modifyTextView.setText("입력");
+                        modifyTextView.setTextColor(0xff000000);
+
                     }
                 });
             }
         }
+
+        cancelLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
 
     }
 
